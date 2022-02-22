29ab0a72f4ac5af9072f368d93b58408008e0202
Ksenia <ksuha280502@gmail.com>
2022-02-22 11:36:13 +0300

    2

--- 3.js
+++ 3.js
@@ -2,5 +2,5 @@ console.log("Задание №3 \n");
 let celsia, farengeit;
 celsia = (66.2 - 32) / 1.8;
 farengeit = 25*1.8 +32;
-console.log ( 25 + "\xB0" + "C cоответсвует " + farengeit + "\xB0" + "F" );
-console.log ( 66.2 + "\xB0" + "F cоответсвует " + celsia + "\xB0" + "C" );
+console.log (`25\xB0C cоответсвует ${farengeit}\xB0F`);
+console.log ( `66.2\xB0F cоответсвует ${celsia}\xB0C`);
